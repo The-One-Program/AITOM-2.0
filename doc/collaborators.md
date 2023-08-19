0a3e354696bcd5f2eb34be70d63784b4a6c1bcfb
mrapoorvaverma31@gmail.com <kshitiz4kaushik@gmail.com>
2023-08-19 19:33:47 +0530

    Initial Commit

---
+++ doc/collaborators.md
@@ -0,0 +1,14 @@
+### Collaborating labs / teams
+
+* [Murphy lab](https://www.andrew.cmu.edu/user/murphy/)
+
+* [Scipion team](http://scipion.i2pc.es/)
+
+### Individual contributors
+
+* [Contributors to the main library](https://github.com/xulabs/aitom/graphs/contributors)
+
+* [Contributors to the tutorials](https://github.com/xulabs/aitom_doc/graphs/contributors)
+
+
+
---
+++ doc/tutorials/001_Load_volume_and_display.py
@@ -0,0 +1,45 @@
+"""
+Load Volume and Display image¶
+change to parent directory to use aitom library
+"""
+
+# load mrc file using io module
+# example data:
+# Bring your packages onto the path
+import sys, os
+sys.path.append(os.path.abspath(os.path.join('..', 'aitom')))
+
+# http://ftp.ebi.ac.uk/pub/databases/empiar/archive/10045/data/ribosomes/AnticipatedResults/Particles/Tomograms/05/IS002_291013_005_subtomo000001.mrc
+import aitom.io.file as IF
+a = IF.read_mrc_data("data/aitom_demo_single_particle_tomogram.mrc")
+
+
+# denoising using gaussian filter for visualization
+from aitom.filter.gaussian import smooth
+a = smooth(a, sigma=8)
+
+# display image using image module
+import aitom.image.vol.util as IVU
+'''
+a_im is a dict:
+    'im': image data, type of numpy.ndarray, elements in [0, 1]
+    'vt': volume data
+'''
+a_im = IVU.cub_img(a)
+print(type(a_im['im']))
+print(a_im['im'].shape)
+print(a_im['im'][1][1])
+
+import matplotlib.pyplot as plt
+plt.imshow(a_im['im'], cmap='gray')
+plt.show()
+print("Here!!")
+# save the figure into a png file
+import aitom.image.io as IIO
+IIO.save_png(m=a_im['im'], name='tmp/a_im.png')
+
+# display image using `image.util.dsp_cub`
+IVU.dsp_cub(a)
+
+# save slices of a into a png file
+IIO.save_cub_img(v=a, name='tmp/a.png')
